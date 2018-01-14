31c900acd08fa1f0190c56b665231062978c83cf
avikbmohan <avikbmohan@gmail.com>
2018-01-14 16:23:01 -0500

    cleaning

--- sqlPractice/sqlZooTutorial1.sql
+++ sqlPractice/sqlZooTutorial1.sql
@@ -1,39 +1,39 @@
-1. Find the country that start with Y
+--1. Find the country that start with Y
 
 SELECT name FROM world
 WHERE name LIKE 'Y%'
 
-2. Find the countries that end with y
+--2. Find the countries that end with y
 
 SELECT name FROM world
 WHERE name LIKE '%Y'
 
-3. Find the countries that contain the letter x
+--3. Find the countries that contain the letter x
 
 SELECT name FROM world
 WHERE name LIKE '%x%'
 
-4. Find the countries that end with 'land'
+--4. Find the countries that end with 'land'
 
 SELECT name FROM world
 WHERE name LIKE '%land'
 
-5. Find the countries that start with C and end with ia
+--5. Find the countries that start with C and end with ia
 
 SELECT name FROM world
 WHERE name LIKE 'c%IA'
 
-6. Find the country that has oo in the name
+--6. Find the country that has oo in the name
 
 SELECT name FROM world
 WHERE name LIKE '%ee%'
 
-7. Find the countries that have three or more a's in the name
+--7. Find the countries that have three or more a's in the name
 
 SELECT name FROM world
 WHERE name LIKE '%a%a%a%'
 
-8. Find the countries that have "t" as the second character.
+--8. Find the countries that have "t" as the second character.
 
 SELECT name FROM world
 WHERE name LIKE '_t%'
